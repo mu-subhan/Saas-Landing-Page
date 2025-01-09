38a15560ce8a416de4d061689816f11bc71787a9
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-01-09 20:58:01 +0500

    remove react-sidedown dependencies and add react collapse and recreate the component Faqitem component

--- src/component/FaqItem.jsx
+++ src/component/FaqItem.jsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { SlideDown } from "react-slidedown";
-import "react-slidedown/lib/slidedown.css";
+import { Collapse } from "react-collapse";
 
 const FaqItem = ({ item, index }) => {
   const [activeId, setActiveId] = useState(null);
@@ -41,11 +40,9 @@ const FaqItem = ({ item, index }) => {
         </div>
       </div>
 
-      <SlideDown>
-        {activeId === item.id && (
-          <div className="body-3 px-7 py-3.5">{item.answer}</div>
-        )}
-      </SlideDown>
+      <Collapse isOpened={activeId === item.id}>
+        <div className="body-3 px-7 py-3.5">{item.answer}</div>
+      </Collapse>
 
       <div
         className={clsx(
@@ -59,4 +56,5 @@ const FaqItem = ({ item, index }) => {
     </div>
   );
 };
+
 export default FaqItem;
